6e5ab6c87776415f933ed91251bb26e6312e2134
vsandberg <sandbergville@hotmail.com>
2023-03-28 21:23:19 +0300

    added windspeed header

--- koodit/main/main.ino
+++ koodit/main/main.ino
@@ -1,9 +1,11 @@
 #include "hum_temp_sensor.h"
 #include "lumen.h"
+#include "windspeed.h"
 
 float temperature ;
 float humidity ;
-int lumen;
+int lumen ;
+float windspee d;
 
 float taulukko[4];
 
@@ -23,13 +25,22 @@ void loop() {
   taulukko[1] = humidity;
   lumen = lumen_measurement();
   taulukko[2] = lumen;
+  windspeed = windspeed_measurement();
+  taulukko[3] = windspeed;
 
 
   Serial.print(taulukko[0]);
+  Serial.print("°C");
   Serial.print(" ");
   Serial.print(taulukko[1]);
+  Serial.print("%");
   Serial.print(" ");
   Serial.println(taulukko[2]);
+  Serial.print("lux");
+  Serial.print(" ");
+  Serial.println(taulukko[3]);
+  Serial.print("m/s");
+  Serial.print(" ");
 
 
 }
