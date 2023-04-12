84fe1ea810d04930dbd40179029e171cd6d91449
Porpo8d <miikka.rantaporhola@gmail.com>
2023-04-12 14:31:02 +0300

    webtestinki

--- koodit/webhost/handler.php
+++ koodit/webhost/handler.php
@@ -1,20 +1,24 @@
 <?php
 $Temp = $_GET['temp'];                  
-$Humidity = $_GET['hum'];               
+$Humidity = $_GET['hum'];   
+$Wind = $_GET['wind'];
+$Lux = $_GET['lux'];
+
 $date = date('d/m/Y h:i:s a', time());  
 
 
-$fileContent = "Temp is ".$Temp."°C and humidity is ".$Humidity."% at ".$date."\n";
+$fileContent = "   ".$Temp."°C      ".$Humidity."%         ".$Lux."        ".$Wind."ms         ".$date."\n";
 
-.
+
 $status = file_put_contents('log.txt',$fileContent,FILE_APPEND);
-$status = file_put_contents('current.txt',$fileContent);
+
 
 
 
 //File writing status
 if($status)
-    echo "Data written successfully.";
+    echo "Data written successfully,";
 else
     echo "Something went wrong!";
+
 ?>
